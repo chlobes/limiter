bedf10a29a1f0df9541480cb7824ab2d997e5d2a
terepy <terepy@protonmail.com>
2020-11-25 03:37:35 +1100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,19 @@ use std::time::{Instant,Duration};
 pub struct Limiter<'a> {
 	wait_time: Duration,
 	last_sleep: Instant,
-	pub slow_function: Box<FnMut(Duration) + 'a>, //when running slow, this function is called with the amount of time the call to Limiter::sleep() was late by
+	pub callback: Box<dyn FnMut(Duration) + 'a>, //when running slow, this function is called with the amount of time the call to Limiter::sleep() was late by
 }
 
 impl<'a> Limiter<'a> {
-	pub fn from_tps(tps: f64, slow_function: Option<Box<FnMut(Duration) + 'a>>) -> Self {
+	pub fn from_tps<F: FnMut(Duration) + 'a>(tps: f64, callback: F) -> Self {
 		let spt = 1.0 / tps;
-
+		
 		if spt.is_sign_negative() || !spt.is_normal() || spt.floor() > u64::max_value() as f64 { panic!("no"); }
-
+		
 		Self {
 			wait_time: Duration::new(spt.floor() as u64, (spt.fract() * 1e9) as u32),
 			last_sleep: Instant::now(),
-			slow_function: slow_function.unwrap_or_else(|| Box::new(default_slow_function) as Box<_>),
+			callback: Box::new(callback),
 		}
 	}
 
@@ -27,7 +27,7 @@ impl<'a> Limiter<'a> {
 		if let Some(t) = self.wait_time.checked_sub(e) {
 			sleep(t);
 		} else {
-			(self.slow_function)(e-self.wait_time);
+			(self.callback)(e-self.wait_time);
 		}
 		self.last_sleep = Instant::now();
 	}
@@ -43,32 +43,4 @@ impl<'a> Limiter<'a> {
 	pub fn frac_time_left(&self) -> Option<f64> {
 		self.time_left().map(|t| ((t.as_secs() as f64 * 10f64.powi(9)) + t.subsec_nanos() as f64) / ((self.wait_time.as_secs() as f64 * 10f64.powi(9)) + (self.wait_time.subsec_nanos() as f64)))
 	}
-}
-
-use std::cmp::{Eq,PartialEq,Ord,PartialOrd};
-
-impl<'a> Eq for Limiter<'a> {}
-
-impl<'a> PartialEq for Limiter<'a> {
-	fn eq(&self, other: &Self) -> bool {
-		self.wait_time.eq(&other.wait_time)
-	}
-}
-
-use std::cmp::Ordering;
-
-impl<'a> Ord for Limiter<'a> {
-	fn cmp(&self, other: &Self) -> Ordering {
-		self.wait_time.cmp(&other.wait_time)
-	}
-}
-
-impl<'a> PartialOrd for Limiter<'a> {
-	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-		self.wait_time.partial_cmp(&other.wait_time)
-	}
-}
-
-fn default_slow_function(slow: Duration) {
-	println!("fell behind by {} seconds, {} nanoseconds",slow.as_secs(),slow.subsec_nanos());
 }
