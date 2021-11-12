e2ddd49fc3724ca627452b1ef51bf671e7767a07
Yujiri <yujiri@disroot.org>
2021-11-12 16:48:33 +0000

    Break long lines and do a lint

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,15 @@ use std::time::{Instant,Duration};
 pub struct Limiter<'a> {
 	wait_time: Duration,
 	last_sleep: Instant,
-	pub callback: Option<Box<dyn FnMut(Duration) + 'a>>, //when running slow, this function is called with the amount of time the call to Limiter::sleep() was late by
+	// when running slow, this function is called with the amount of time the call to Limiter::sleep() was late by
+	pub callback: Option<Box<dyn FnMut(Duration) + 'a>>,
 }
 
 impl<'a> Limiter<'a> {
 	pub fn from_tps(tps: f64) -> Self {
 		let spt = 1.0 / tps;
 		
-		if spt.is_sign_negative() || !spt.is_normal() || spt.floor() > u64::max_value() as f64 { panic!("no {}",spt); }
+		assert!(!spt.is_sign_negative() && spt.is_normal() && spt.floor() <= u64::max_value() as f64, "no {}", spt);
 		
 		Self {
 			wait_time: Duration::new(spt.floor() as u64, (spt.fract() * 1e9) as u32),
@@ -40,6 +41,8 @@ impl<'a> Limiter<'a> {
 	}
 	
 	pub fn frac_time_left(&self) -> Option<f64> {
-		self.time_left().map(|t| ((t.as_secs() as f64 * 10f64.powi(9)) + t.subsec_nanos() as f64) / ((self.wait_time.as_secs() as f64 * 10f64.powi(9)) + (self.wait_time.subsec_nanos() as f64)))
+		self.time_left().map(|t|
+			((t.as_secs() as f64 * 10f64.powi(9)) + t.subsec_nanos() as f64) /
+			((self.wait_time.as_secs() as f64 * 10f64.powi(9)) + (self.wait_time.subsec_nanos() as f64)))
 	}
 }
