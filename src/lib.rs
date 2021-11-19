5dfcb668ca5fcf2c0a1c52d28732576c46b550c1
terepy <terepy@protonmail.com>
2021-11-19 10:29:03 +0000

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -11,43 +11,40 @@ impl<'a> Limiter<'a> {
 	/// create a `Limiter` given a number of times per second.
 	pub fn from_tps(tps: f64) -> Self {
 		let spt = 1.0 / tps;
-		
-		assert!(!spt.is_sign_negative() && spt.is_normal() && spt.floor() <= u64::max_value() as f64, "no {}", spt);
+		assert!(!spt.is_sign_negative() && spt.is_normal() && spt * 1e9 <= u64::max_value() as f64,
+			"Limiter::from_tps called with invalid value: {} (generates sleep time of {})",tps,spt);
 		
 		Self {
-			wait_time: Duration::new(spt.floor() as u64, (spt.fract() * 1e9) as u32),
+			wait_time: Duration::from_nanos((spt * 1e9) as u64),
 			last_sleep: Instant::now(),
 			callback: None,
 		}
 	}
-
-	/// wait until it's time for the next execution.
+	
+	/// sleep until it's time for the next execution.
 	pub fn sleep(&mut self) {
 		let e = self.last_sleep.elapsed();
-		
-		if let Some(t) = self.wait_time.checked_sub(e) {
+		if let Some(t) = self.wait_time.checked_sub(e) { //sleep was called in time
 			std::thread::sleep(t);
-		} else {
+		} else { //we are running late
 			let t = e-self.wait_time;
 			self.callback.as_mut().map(|f| f(t));
 		}
 		self.last_sleep = Instant::now();
 	}
-
-	/// restart the timer until next execution.
+	
+	/// restart the timer without sleeping.
 	pub fn reset(&mut self) {
 		self.last_sleep = Instant::now();
 	}
-
-	/// returns `None` if it is past time for the next execution.
+	
+	/// time left until next sleep, if any.
 	pub fn time_left(&self) -> Option<Duration> {
 		self.wait_time.checked_sub(self.last_sleep.elapsed())
 	}
 	
-	/// returns `None` if it is past time for the next execution.
+	/// time left as a fraction of total time, if any.
 	pub fn frac_time_left(&self) -> Option<f64> {
-		self.time_left().map(|t|
-			((t.as_secs() as f64 * 10f64.powi(9)) + t.subsec_nanos() as f64) /
-			((self.wait_time.as_secs() as f64 * 10f64.powi(9)) + (self.wait_time.subsec_nanos() as f64)))
+		self.time_left().map(|t| t.as_nanos() as f64 / self.wait_time.as_nanos() as f64)
 	}
 }
